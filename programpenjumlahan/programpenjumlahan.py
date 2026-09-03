1a7ba3fb13241901c5c214a9c0b83fa18478566f
Ryan <ryansyaputra060608@gmail.com>
2026-09-03 17:27:40 +0700

    upload folder programpenjumlahan

---
+++ programpenjumlahan/programpenjumlahan.py
@@ -0,0 +1,13 @@
+#program kalkulator pertambahan
+
+print("\nPROGRAM PENJUMLAHAN\n")
+
+a = float(input('masukkan angka:'))
+print("X=", a)
+b = float(input('masukkan angka:'))
+print("Y=", b)
+c = float(input('masukkan angka:'))
+print("Y=", c)
+
+jumlah = a + b + c
+print("hasil=", jumlah)
