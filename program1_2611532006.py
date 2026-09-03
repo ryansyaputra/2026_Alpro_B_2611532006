3865f7b78758801fd5a258cec576b47336ba3a51
ryansyaputra <ryansyaputra060608@gmail.com>
2026-09-03 10:00:29 +0700

    Add files via upload

---
+++ program1_2611532006.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello Word")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\n"
++ "Ryan Syaputra dan 2611532006")
